3afbc792a09002a024cf05db4f52a8df79306a9d
Boston <bostoncessary@gmail.com>
2023-10-08 16:57:05 -0700

    add more basic styling and additions to weather card

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const KEY = '?key=0be96db5acc6427f81c173514230510'
 const BASE_URL = 'https://api.weatherapi.com/v1'
 const CURRENT_WEATHER = '/current.json'
+const FUTURE_WEATHER = '/forecast.json'
 let userLocation = '&q='
 let fTemp = true
 const area = document.querySelector('.location')
@@ -12,6 +13,11 @@ const formContainer = document.getElementById('form-container')
 const weatherCard = document.getElementById("weather-card")
 const condition = document.querySelector(".condition")
 const conditionIcon = document.querySelector('.condition-icon')
+const timeContainer = document.querySelector('.time')
+const locationIcon = document.querySelector(".location-icon")
+const clockIcon = document.querySelector(".clock-icon")
+const highTemp = document.querySelector(".high-temp")
+const lowTemp = document.querySelector(".low-temp")
 const form = document.querySelector('.form')
 const nav = document.getElementById('nav')
 
@@ -20,6 +26,7 @@ const nav = document.getElementById('nav')
 submit.addEventListener("click", function(e){
   e.preventDefault()
   fetchData()
+  fetchForecastData()
   weatherCard.classList.remove("hidden")
 })
 
@@ -35,63 +42,25 @@ changeTempButton.addEventListener("click", function(){
   console.log(fTemp)
 })
 
-function changeBGColorFahren(climate){
+function changeBGColorFahren(isDay){
   weatherCard.removeAttribute('class')
-  
-  if(climate >= 100){
-    weatherCard.classList.add('hot')
-  }
-  else if (climate >= 80){
-    weatherCard.classList.add('sunny')
-    
-    
-
-  }
-  else if(climate >= 50){
-    weatherCard.classList.add('cool')
-
+  if (isDay === 1){
+    weatherCard.classList.add("sunny")
   }
   else{
-    weatherCard.classList.add('cold')
-
-  }
-}
-
-function changeBGColorFahrenNight(climate){
-  weatherCard.removeAttribute('class')
-  
-  if(climate >= 100){
-    weatherCard.classList.add('hot')
-  }
-  else if (climate >= 80){
-    weatherCard.classList.add('sunny')
-    
-    
-
-  }
-  else if(climate >= 50){
     weatherCard.classList.add('cool-night')
-
-  }
-  else{
-    weatherCard.classList.add('cold')
-
   }
 }
 
-function changeBGColorCelsius(climate){
-  document.body.removeAttribute('class')
-  if(climate >= 38){
-    document.body.classList.add('hot')
-  }
-  else if (climate >= 27){
-    document.body.classList.add('sunny')
-  }
-  else if(climate >= 10){
-    document.body.classList.add('cool')
+
+
+function changeBGColorCelsius(isDay){
+  weatherCard.removeAttribute('class')
+  if (isDay === 1){
+    weatherCard.classList.add("sunny")
   }
   else{
-    document.body.classList.add('cold')
+    weatherCard.classList.add('cool-night')
   }
 }
 
@@ -121,7 +90,7 @@ function changeIconNight(response){
     conditionIcon.src = "icons/starry-night.svg"
   }
   else if(response.current.condition.text === "Partly cloudy"){
-    conditionIcon.src = "icons/partly-cloudy-day.svg"
+    conditionIcon.src = "icons/partly-cloudy-night.svg"
   }
   else if(response.current.condition.text === "Clear"){
     conditionIcon.src = "icons/starry-night.svg"
@@ -135,6 +104,43 @@ function changeIconNight(response){
   else if(response.current.condition.text === "Overcast" ){
     conditionIcon.src = "icons/overcast.svg"
   }
+  else if(response.current.condition.text === "Light rain"){
+    conditionIcon.src = "icons/partly-cloudy-night-drizzle.svg"
+  }
+}
+
+
+
+function converTime(time){
+  let isAm = true
+  let currentTime = time.substr(11)
+  console.log(currentTime)
+  let standDardTime = ''
+  currentTime = currentTime.split(':')
+  
+  let hour = Number(currentTime[0])
+  let minute = currentTime[1]
+
+  console.log(currentTime)
+  if (hour <= 12 && hour > 0){
+    standDardTime = "" + hour
+  }
+  else if(hour > 12){
+    standDardTime = '' + (hour - 12)
+    isAm = false;
+  }
+  else if(hour === 0){
+    standDardTime = "" + 12
+  }
+
+  if (isAm === true){
+    standDardTime = standDardTime + (':' + minute) +' am'
+  }
+  else{
+    standDardTime = standDardTime + (':' + minute) + ' pm'
+  }
+
+  return standDardTime
 }
 
 function fetchData(){
@@ -146,26 +152,54 @@ function fetchData(){
     return response.json()
   })
   .then(function(response) {
-  
+
+    let time = response.location.localtime
+    timeContainer.textContent = converTime(time)
     console.log(response)
-    console.log(response.current.is_day)
+    
+   
     area.textContent = response.location.name + ', ' + response.location.region
     condition.textContent = response.current.condition.text
     if(fTemp === true ){
-      temp.textContent = 'Temp: ' + response.current.temp_f + ' Fº'
+      temp.textContent = Math.round(response.current.temp_f) + ' F°'
       if(response.current.is_day === 1){
-        changeBGColorFahren(response.current.temp_f)
+        changeBGColorFahren(response.current.is_day)
+        locationIcon.src = 'icons/location-svgrepo-com.svg'
+        clockIcon.src = "icons/icons8-clock.svg"
+        if(weatherCard.classList.contains("light-text")){
+          weatherCard.classList.remove("light-text")
+        }
       }
       else{
-        changeBGColorFahrenNight(response.current.temp_f)
+        locationIcon.src = 'icons/location-white.svg'
+        clockIcon.src = "icons/clock-white.svg"
+        changeBGColorFahren(response.current.is_day)
+        weatherCard.classList.add("light-text")
       }
       
 
     }
     else{
-      temp.textContent = response.current.temp_c + ' Cº'
-      changeBGColorCelsius(response.current.temp_c)
+      temp.textContent = Math.round(response.current.temp_c) + ' C°'
+      if(response.current.is_day === 1) {
+        changeBGColorCelsius(response.current.is_day)
+        locationIcon.src = 'icons/location-svgrepo-com.svg'
+        clockIcon.src = "icons/icons8-clock.svg"
+        if(weatherCard.classList.contains("light-text")){
+          weatherCard.classList.remove("light-text")
+        }
+      }
+      else{
+        locationIcon.src = 'icons/location-white.svg'
+        clockIcon.src = "icons/clock-white.svg"
+        changeBGColorCelsius(response.current.is_day)
+        weatherCard.classList.add("light-text")
+      }
+      
     }
+
+    
+    
 
     console.log(response.current.is_day)
     if(response.current.is_day === 1){
@@ -177,4 +211,23 @@ function fetchData(){
     
       
   });
+
+  
+}
+
+function fetchForecastData(){
+  let value = document.getElementById("zipcode").value
+  let zip = userLocation.concat(value)
+
+  fetch(`${BASE_URL}${FUTURE_WEATHER}${KEY}${zip}`, {mode: 'cors'})
+.then(function(response){
+  return response.json()
+})
+.then(function(response) {
+  const todayHigh = Math.round(response.forecast.forecastday[0].day.maxtemp_f)
+  const todayLow = Math.round(response.forecast.forecastday[0].day.mintemp_f)
+  
+  highTemp.textContent = "H " + todayHigh
+  lowTemp.textContent = "L " + todayLow
+});
 }
